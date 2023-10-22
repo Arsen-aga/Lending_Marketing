4c4389d79ee1de24fd7367d78f7ec4e05922c398
Arsen Asanov <tatarin08082002@gmail.com>
2023-10-22 19:07:37 +0300

    latest blocks added

--- js/main.js
+++ js/main.js
@@ -4,4 +4,9 @@ $(function () {
 
   Fancybox.bind("[data-fancybox]", {});
 
+  $('.slider-blog__inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="left"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="right"></button>'
+  })
 });
